db9c3739bf9fb8aa17e85c4fde54d8b51ce4bebb
JunXiang-Leong <72239762+JunXiang-Leong@users.noreply.github.com>
2024-07-12 14:48:51 +0800

    added support for argc & argv

--- CppUnitTest/Source.cpp
+++ CppUnitTest/Source.cpp
@@ -3,17 +3,34 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <iostream>
 CPPUNIT_TEST_SUITE_REGISTRATION(LibmonTest);
-int main()
+int main(int argc, char* argv[])
 {
 	CppUnit::TextUi::TestRunner runner;
 	CppUnit::TestFactoryRegistry& registry = CppUnit::TestFactoryRegistry::getRegistry();
 	runner.addTest(registry.makeTest());
 	bool result = runner.run();
-	LibmonTest::SetBounds(11,32);
-	result = runner.run();
-	if (result)
-		printf("all tests passed!\n");
+
+	if (argc > 1)
+	{
+		if ((argc - 1) % 2 != 0) //first value of argv is the exe itself
+		{
+			std::cerr << "Incorrect amount of arguments" << std::endl;
+			return 0;
+		}
+		for (int i = 0; i < (argc-1) / 2; ++i)
+		{
+			S16BIT lower = std::stoi(argv[i*2 + 1]); 
+			S16BIT upper = std::stoi(argv[i * 2 + 2]);
+
+			LibmonTest::SetBounds(lower, upper);
+			result &= runner.run();
+		}
+	}
+	
+	if (!result)
+		std::cerr << "Test Failed" << std::endl;
+	else
+		std::cout << "Test Completed" << std::endl;
 	int a = 0;
-	std::cin >> a;
 	return 0;
 }
